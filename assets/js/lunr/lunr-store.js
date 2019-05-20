98b1dfbae45ebdae6397c0afb901eee8cd89ef02
bowenwen <bowenwen@users.noreply.github.com>
2019-05-19 22:41:02 -0700

    add bigfoot, dynamic footnote support

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,54 +1,11 @@
----
-layout: null
----
-
-var store = [
-  {%- for c in site.collections -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
-    {%- for doc in docs -%}
-      {%- if doc.header.teaser -%}
-        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
-      {%- else -%}
-        {%- assign teaser = site.teaser -%}
-      {%- endif -%}
-      {
-        "title": {{ doc.title | jsonify }},
-        "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | jsonify }},
-          {%- else -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endif -%}
-        "categories": {{ doc.categories | jsonify }},
-        "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | absolute_url | jsonify }},
-        "teaser":
-          {%- if teaser contains "://" -%}
-            {{ teaser | jsonify }}
-          {%- else -%}
-            {{ teaser | absolute_url | jsonify }}
-          {%- endif -%}
-      }{%- unless forloop.last and l -%},{%- endunless -%}
-    {%- endfor -%}
-  {%- endfor -%}]
+var store = [{
+        "title": "An introduction to Braess' Paradox",
+        "excerpt":"Tl’dr: Usually, additional roads improve congestion. However, in certain circumstances, more roads can lead to worse congestion. It is important to assess the impact of new infrastructure! I first learned about Braess’ Paradox in a third year transportation engineering class. It is a simple exercise to demonstrate the importance of...","categories": ["Mobilities"],
+        "tags": ["math","traffic"],
+        "url": "http://localhost:4000/mobilities/intro-braess-paradox/",
+        "teaser":null},{
+        "title": "User Equilibrium vs. System Optimal",
+        "excerpt":"John Glen Wardrop was the famous English mathematician who first proposed the idea of user equilibrium and system optimal conditions in traffic assignment 1. His idea was closely related to the idea of Nash Equilibrium, which was separately HTML, CSS, and JavaScript files along with images and other assets needed...","categories": ["Mobilities"],
+        "tags": ["math","traffic"],
+        "url": "http://localhost:4000/mobilities/ue-vs-so/",
+        "teaser":null}]
